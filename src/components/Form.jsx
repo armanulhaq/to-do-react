c18e3206abbb0f147f0f9f23c62eb678d1c27792
Arman Ul Haq <72503393+armanulhaq@users.noreply.github.com>
2024-11-07 15:30:10 +0530

    add completion functionality

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,20 +2,22 @@ import styles from "./form.module.css";
 import { useState } from "react";
 
 const Form = ({ todoItems, setTodoItems }) => {
-    const [todoItem, setTodoItem] = useState("");
+    const [todoItem, setTodoItem] = useState({ name: "", done: false });
 
     function handleSubmit(e) {
         e.preventDefault();
         setTodoItems([...todoItems, todoItem]); // Add new todo item
-        setTodoItem("");
+        setTodoItem({ name: "", done: false });
     }
 
     return (
         <form className={styles.todoForm} onSubmit={handleSubmit}>
             <input
                 className={styles.todoInput}
-                onChange={(e) => setTodoItem(e.target.value)}
-                value={todoItem}
+                onChange={(e) =>
+                    setTodoItem({ name: e.target.value, done: false })
+                }
+                value={todoItem.name}
                 placeholder="Enter todos"
                 type="text"
             />
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,15 +2,29 @@ import styles from "./todoitem.module.css";
 
 export default function TodoItem({ item, todos, setTodoItems }) {
     function handleDelete() {
-        console.log("Delete button clicked for item", item);
-        // Use todos instead of item and filter out the current item
         setTodoItems(todos.filter((todoItem) => todoItem !== item));
     }
+    function handleClick(name) {
+        let newTodos = todos.map((todo) =>
+            todo.name === name ? { ...todo, done: !todo.done } : todo
+        );
+        setTodoItems(newTodos);
+        console.log(newTodos); // Log the updated todos instead of the original
+    }
+
+    const isCompleted = item.done ? styles.completed : "";
 
     return (
         <>
             <div className={styles.item}>
-                <div className={styles.itemName}>{item}</div>
+                <div className={styles.itemName}>
+                    <span
+                        className={isCompleted}
+                        onClick={() => handleClick(item.name)}
+                    >
+                        {item.name}
+                    </span>
+                </div>
                 <button
                     onClick={handleDelete} // No need to pass 'item' here
                     className={styles.deleteButton}
